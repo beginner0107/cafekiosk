f04dd3a5eb4ad38f5ae59d2994d0c7a7f691c61d
Beginner <dkstmdwn17@naver.com>
2023-12-10 13:22:55 +0900

    Initial commit

---
+++ src/main/java/sample/cafekiosk/unit/order/Order.java
@@ -0,0 +1,13 @@
+package sample.cafekiosk.unit.order;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import sample.cafekiosk.unit.beverage.Beverage;
+
+@RequiredArgsConstructor
+public class Order {
+
+  private final LocalDateTime orderDateTime;
+  private final List<Beverage> beverages;
+}
