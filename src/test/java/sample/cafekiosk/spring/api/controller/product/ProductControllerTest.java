ea9e067bec1b2aa1edd9f6ec0a999246a07111cc
Beginner <dkstmdwn17@naver.com>
2023-12-22 00:32:58 +0900

    Presentation Layer 테스트(2) - 상품타입 NULL값 테스트

--- src/test/java/sample/cafekiosk/spring/api/controller/product/ProductControllerTest.java
+++ src/test/java/sample/cafekiosk/spring/api/controller/product/ProductControllerTest.java
@@ -2,6 +2,7 @@ package sample.cafekiosk.spring.api.controller.product;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import sample.cafekiosk.spring.api.service.product.ProductService;
@@ -44,6 +46,29 @@ class ProductControllerTest {
         .content(objectMapper.writeValueAsString(request))
         .contentType(APPLICATION_JSON)
     )
-        .andExpect(MockMvcResultMatchers.status().isOk());
+        .andExpect(status().isOk());
+  }
+
+  @DisplayName("신규 상품을 등록할 때 상품 타입은 필수적이다.")
+  @Test
+  void createProductWithoutType() throws Exception {
+    // given
+    ProductCreateRequest request = ProductCreateRequest.builder()
+        .type(null)
+        .sellingStatus(ProductSellingStatus.SELLING)
+        .name("아메리카노")
+        .price(4000)
+        .build();
+
+    // when & then
+    mockMvc.perform(post("/api/v1/products/new")
+            .content(objectMapper.writeValueAsString(request))
+            .contentType(APPLICATION_JSON)
+        )
+        .andExpect(status().isBadRequest())
+        .andExpect(jsonPath("$.code").value("400"))
+        .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+        .andExpect(jsonPath("$.message").value("상품 타입은 필수입니다."))
+        .andExpect(jsonPath("$.data").isEmpty());
   }
 }
