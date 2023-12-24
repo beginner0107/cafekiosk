41a0438b17816be368771a852a6e7d906b932a62
Beginner <dkstmdwn17@naver.com>
2023-12-24 21:55:03 +0900

    Mockito로 Stubbing하기 - 주석 삭제

--- src/main/java/sample/cafekiosk/spring/api/service/mail/MailService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/mail/MailService.java
@@ -17,7 +17,7 @@ public class MailService {
 
     boolean result = mailSendClient.sendEmail(fromEmail, toEmail, subject, content);
     if (result) {
-      mailSendHistoryRepository.save(MailSendHistory.builder()
+      MailSendHistory history = mailSendHistoryRepository.save(MailSendHistory.builder()
           .fromEmail(fromEmail)
           .toEmail(toEmail)
           .subject(subject)
