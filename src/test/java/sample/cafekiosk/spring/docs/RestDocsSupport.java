a7cbc0ea78a6015ff7178d991dd8a24b05e99f19
Beginner <dkstmdwn17@naver.com>
2023-12-25 23:02:25 +0900

    RestDocs webAppContextSetup 방식을 쓸 때 @SpringBootTest 추가

--- src/test/java/sample/cafekiosk/spring/docs/RestDocsSupport.java
+++ src/test/java/sample/cafekiosk/spring/docs/RestDocsSupport.java
@@ -2,6 +2,7 @@ package sample.cafekiosk.spring.docs;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
@@ -10,6 +11,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 @ExtendWith(RestDocumentationExtension.class)
+@SpringBootTest
 public abstract class RestDocsSupport {
 
   protected MockMvc mockMvc;
